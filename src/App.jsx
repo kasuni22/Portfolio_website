4564f2a4281d9473f18e1fcddd36a7c46777aa84
Kasuni Kariyawasam <kmkariyawasam4@gmail.com>
2026-03-15 17:05:54 +0530

    Add dark mode and Create Nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
+import { useEffect, useState } from 'react'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+import Navbar from "./components/Navbar"
 
 const App = () => {
+  const [darkMode, setDarkMode] = useState(true)
+
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+      once: false,
+      offset: 100
+
+    });
+    document.documentElement.classList.add('dark');
+  }, []);
+
+  const toggleDarkMode = () => {
+    const newMode = !darkMode;
+    setDarkMode(newMode);
+    document.documentElement.classList.toggle('dark');
+  };
   return (
-    <div>
-      <h1 class="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+    <div className={
+      darkMode
+        ? 'bg-gradient-to-br from-gray-900 via-[#0d182e] to-gray-900 min-h-screen'
+        : 'bg-gradient-to-br from-gray-50 to-gray-50 min-h-screen'
+    }>
+      <Navbar darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
     </div>
   )
 }
